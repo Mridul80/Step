41f8f47a5ce3f5dd78574ec12b0105901ab0a9bc
Mridul80 <mk1265@srmist.edu.in>
2026-02-24 10:29:34 +0530

    Base application settup

---
+++ src/PalindromeApp.java
@@ -0,0 +1,11 @@
+
+public class PalindromeApp {
+
+    public static void main(String[] args) {
+
+        // Display Welcome Message
+        System.out.println("Welcome to the Palindrome Checker Management System");
+        System.out.println("Version : 1.0");
+        System.out.println("System initialized successfully");
+    }
+}
